caed2ff625f9ee66b1a6a98038ceb95b0b2afadd
chaogao <chaogao2016@outlook.com>
2017-07-25 20:33:59 +0800

    第五天

---
+++ ex28.py
@@ -0,0 +1,35 @@
+print True and True , " is true"
+
+print False and True , " is false"
+
+print 1 == 1 and 2 == 1, " is false"
+
+print "test" == "test", " is true"
+
+print 1 == 1 or 2 != 1, " is true"
+
+print True and 1 == 1, " is true"
+
+print False and 0 != 0, " is false"
+
+print True or 1 == 1, " is true"
+
+print "test" == "testing", " is false"
+
+print "test" == 1, " is false"
+
+print not (True and False) , "is true"
+
+print not (1 == 1 and 0 != 1), " is false"
+
+print not (10 == 1 or 1000 == 1000)
+
+print not (1 != 10 or 3 == 4) , " is false"
+
+print not ('testing' == "testing" and "Zed" == "Cool Guy")," is true"
+
+print 1 == 1 and not ("testing" == 1 or 1 == 0), " is true"
+
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3), " is false"
+
+print 3 == 3 and not ("testing" == "testing" or "Python" == "Fun"), " is false"
